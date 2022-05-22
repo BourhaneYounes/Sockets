b593cb6dea2ec489258ab410880874bf0efb52ae
Bourhane Younes <bourhaneyounes1@gmail.com>
2022-05-22 20:30:20 +0100

    typo and compatibility with server

--- client.c
+++ client.c
@@ -8,12 +8,12 @@
 #include<stdlib.h>
 #include<unistd.h>
 #include<errno.h>
-#include<pthread.h>
+
 
 int main(void){
 
     int socketClient = socket(AF_INET, SOCK_STREAM, 0), bytes_recv;
-    char send_data[1024], rcv_data[11];
+    char send_data[6], rcv_data[6];
     struct sockaddr_in addrServer; 
     addrServer.sin_addr.s_addr = inet_addr("127.0.0.1");
     addrServer.sin_family = AF_INET;
@@ -22,8 +22,11 @@ int main(void){
     connect(socketClient, (const struct sockaddr*)&addrServer, sizeof(addrServer));
     printf("connection\n");
 
-    recv(socketClient, rcv_data, 10, 0);
+    recv(socketClient, rcv_data, 6, 0);
     printf("%s", rcv_data);
+    
+    scanf("%s",send_data);
+    send(socketClient, send_data, sizeof(send_data), 0);
 
     close(socketClient);
     
